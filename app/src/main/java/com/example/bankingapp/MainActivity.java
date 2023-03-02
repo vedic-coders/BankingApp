1b6792821746870160ba1ec5d89782129c9af0b9
Varun Kumar <varunkr2109@gmail.com>
2023-03-02 20:31:14 +0530

    Commit by Varun

--- app/src/main/java/com/example/bankingapp/MainActivity.java
+++ app/src/main/java/com/example/bankingapp/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.bankingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.txt);
+    }
+
+    public void ohYeah(View view)
+    {
+        textView.setText("Oh Yeah");
+        textView.setTextSize(100);
+        textView.setTextColor(getResources().getColor(R.color.red));
     }
 }
